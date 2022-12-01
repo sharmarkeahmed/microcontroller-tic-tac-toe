2cc29cb93c9db735b606f9e9e89a6e871f1e4c13
Sharmarke Ahmed <101375936+sharmarkeahmed@users.noreply.github.com>
2022-11-30 23:31:46 -0600

    Update player1.ino
    Added Particle.publish, still need to finish Particle.subscribe to finish the code

--- player1.ino
+++ player1.ino
@@ -1,5 +1,4 @@
 // Final Project Code for PLAYER1
-// NOTE: CODE IS INCOMPLETE
 
 /*
 IMPORTANT: Import the neopixel and Keypad_Particle.h libraries in order to use this code
@@ -39,6 +38,7 @@ Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 // Declaring & initializing iLED Colors. Note: WS2812 color code is in form strip.Color(Green, Red, Blue)
 int PixelColorRed = strip.Color(0 , 255, 0); // Red LED color (to be used for PLAYER 1)
 int PixelColorBlue = strip.Color(0, 0, 255); // Blue LED Color (to be used for PLAYER 2)
+int PixelColorGreen = strip.Color(255, 0, 0); // Green LED Color (to be used in the case of a tie to blink all LED's a few times, see blinkAllLeds())
 int PixelColorOff = strip.Color(0 , 0, 0); // Declaration & initialization of no LED color (off)
 
 // Multidimensional arrays to store the state of the players. 
@@ -47,16 +47,20 @@ bool P1State[3][3];
 bool P2State[3][3];
 
 // STATE VARIABLES ASSOCIATED WITH PUBLISHING
-bool waitingToPublish = false; // If true, Particle Photon will wait at least 1 second before trying to publish something
+bool waitingToPublish = false; // If true, Particle Photon will wait to publish
 bool publishNewChange = false; // If true, particle photon will publish a new change
 bool publishGameReset = false; // If true, particle photon will publish a game reset
-bool wait = false; // Once 1 second has elapsed, this will become false.
+bool publishGameWin = false; // // If true, particle photon will publish a game win
+bool wait = false; // Once 1 second has elapsed, this will become false. Used to ensure that particle photon waits one second to publish something. 
+String winData; // Data to store information about the win.
+
 
 // OTHER STATE VARIABLES
 bool resetGame = true;
 bool player1Playing = true; // When the game first starts, player 1 will go first
 bool player2Playing = false; // When the game first starts, player 2 will go second
 bool updateGame = false; // Initially this is false, but if this is true the states of the other player has changed, so this microcontroller will also have to change as well. 
+
 
 int ledTurnedOnP1; // LED to turn on (P1)
 int ledTurnedOnP2; // LED that P2 Turned on
@@ -69,10 +73,19 @@ int p1ColChange;
 int p2RowChange;
 int p2ColChange;
 
+// Winning LED Numbers will be stored in this array
+int ledWinningNumbers[3];
+
+int iLED; // Will hold the iLED number that is turned on
+
 
 // Function Declarations
 void startNewGame();
-void turnOnLED(char key);
+int turnOnThisLED(char key);
+int detectWin();
+void blinkLED();
+void blinkAllLEDS();
+
 // Handler Function Declarations
 
 unsigned long int waitingTime; // The amount of time to wait
@@ -85,25 +98,31 @@ void setup() {
 void loop() {
     unsigned long int currentTime = millis();  // get the current time
 
-    // In order to wait 1 second before publishing, a timer is used to avoid blocking code. NOTE: User cannot request a game reset for one second after pressing button. 
+    // In order to wait 1 second before publishing, a timer is used to avoid blocking code. Timer only matters when a publish is request (although timer will still continue to work so that its ready to go when needed)
     if (currentTime > waitingTime) {
-        // Ready to publish (if the photon is waiting to publish)
+        // One second has elapsed, we are now ready to publish (if the photon is waiting to publish)
         wait = false;
         waitingTime += 1000;
     }
+    /* Note on requesting a publish: Please initialize wait to true and waitingToPublish to true in order to properly request a publish, otherwise weird things may begin to happen. 
+     Likewise, waitingtoPublish should be initialized to false when done publishing something
+     Also note the difference between wait and waitingToPublish, wait is initialied from true to false once one second has elapsed indicating a safe publish, waitingToPublish just blocks the code from doing other things until
+     the publishing has happened. When waiting for a publish, the only things the photon will be doing will be checking for a reset request or seeing if the other photon has published anything.
+    */
 
+    char key = keypad.getKey(); // This key will store the value that the user pressed
 
-    if (waitingToPublish == false) {
-
-        char key = keypad.getKey(); // This key will store the value that the user pressed
-
-        // If the user presses the pound key, the game will be reset.
+    // If the user presses the pound key, the game will be reset.
         if (key == '#') {
             publishGameReset = true; // Need to publish a game reset before actually resetting the game to make sure other particle photon resets their game board as well
-            wait = true;
-            waitingToPublish = true;
+            wait = true; // Photon must now wait one second
+            waitingToPublish = true; // Indicates that photon is waiting for a publish
         }
-        
+
+    // FIXME: Add Particle.subscribe stuff here
+
+
+    if (waitingToPublish == false) { // If the photon is not waiting to publish something
         // If a user requests that the game should be reset, resetGame will be true for both particle photon's
         if (resetGame == true) {
             startNewGame();
@@ -124,32 +143,78 @@ void loop() {
 
             if (key) {
                 // This if statement will only run if player 1 presses a key
-                int iLED = turnOnThisLED(key); // iLED will store the iLED that should be turned on
+                iLED = turnOnThisLED(key); // iLED will store the iLED that should be turned on
+                // winStatus initialized to -2 if all iLED's on and both players lost, or 1 if a player has won a game, or -1 if neither players have yet to win for this round iteration
+                int winStatus = detectWin(); 
+
 
                 // If an iLED should turn on
-                if (iLED != -1) {
+                if (winStatus == 1) {
+                    // A player has won a game. 
+
+                    // Particle photon will wait to publish this.
+                    winData = "W"; // The data will begin with W when published (see publishing section of loop() ), indicating a win from P1
+                    publishGameWin = true;
+                    waitingToPublish = true;
+                    wait = true;
+                }
+                else if (winStatus == -2) {
+                    // Neither player has won the game and all iLED's are on.
+
+                    // Particle photon will wait to publish this
+                    winData = "L"; // The data will begin with L when published (see publishing section of loop() ), indicating a loss for both players
+                    publishGameWin = true;
+                    waitingToPublish = true;
+                    wait = true;
+                }
+                else if (iLED != -1) { // This else if statement will run if an iLED should be turned on and if nobody has won/lost yet
                     strip.setPixelColor(iLED, PixelColorRed); // Turns on the iLED
                     waitingToPublish = true; // Photon will start waiting to publish
                     publishNewChange = true; // Ready to publish this new change!
                     wait = true; // Will ensure that at least 1 second will be waited for until particle publishing
                 }
-
+                // Neopixels will not light up until publishing section of code, to ensure that both photons light up at the same time
             } 
         }
 
     }
-    else {
-        if (wait == false) {
-        // This contains every particle.publish() that this photon will do. 
+    else { 
+        // If the photon IS waiting to publish
+        if (wait == false) { // If one second has elapsed (otherwise, continue waiting!)
+        // This contains every particle.publish() that this photon could do. 
             if (publishNewChange == true) {
-                // FIXME: Must do a particle publish to both indicate it is now the other particle photon's turn to play the game.
+                String data;
+                data = iLED;
+                data += p1RowChange;
+                data += p1ColChange;
+                Particle.publish("P1StateChange", data);
+                // Data will contain (in order) the iLED value, the p1RowChange, the p1ColChange value for P2 to update their valus.
+                // We are done publishing, so must set these values to false as well
                 waitingToPublish = false;
+                publishNewChange = false;
             }
             else if (publishGameReset == true) {
-                // FIXME: Must do a particle publish to indicate a game reset
-
+                Particle.publish("P1ResetGameRequest", "");
+                // Notify P2 that there was a reset game request
+                resetGame = true; // P1 can now reset the game
+                // We are done publishing, so must set these values to false as well
+                waitingToPublish = false;
+                publishGameReset = false;
             }
-            strip.show(); // updates iLED's 
+            else if (publishGameWin == true) {
+                if (winData[0] == 'W') {
+                for (int i = 0; i < 3; i++) {
+                // If a win has occured, then the publish data will start with 'W' and contain the three winning numbers. That way, both photons can blink the winning numbers at the same time.
+                // If a win has not occured, winData will only have "L" when published.
+                winData += ledWinningNumbers[i];
+                }
+                }
+                Particle.publish("P1GameWin", winData);
+                // We are done publishing, so must set these values to false as well
+                waitingToPublish = false;
+                publishGameWin = false;
+            }
+            strip.show(); // updates iLED's after the other photon has been notified (so both change colors at the same time).
         }   
         
     }
@@ -256,8 +321,6 @@ int turnOnThisLED(char key) {
             return -1;
         }
     }   
-    // ------------
-    // FIXME: Finish the rest of this code later on when 3x3 matrices are supported
     else if (key == '6') {
         if (P1State[1][2] == false && P2State[1][2] == false) {
             P1State[1][2] = true;
@@ -311,3 +374,140 @@ int turnOnThisLED(char key) {
 
     return -1; // This will prevent some error from happening if for some reason the code escapes the if statements
 }
+/*Description: This function will detect whether or not the player has won the game.
+Inputs:
+    N/A
+Outputs:
+    1 - If player has won a game
+    -1 - If player has not won a game
+    -2 - If neither player wins after all LED's are on
+Global Variables Used:
+    ledWinningNumbers - changes the individual values of the array so that the program will know what LED's to blink later on. 
+    P1State & P2State - uses the state of the two players to determine whether or not a win has occured
+*/
+int detectWin() {
+    if(P1State[0][0] == P1State[0][1] && P1State[0][0] == P1State[0][2]){
+ 
+            ledWinningNumbers[0] = 0;
+            ledWinningNumbers[1] = 1;
+            ledWinningNumbers[2] = 2;
+            return 1;
+ 
+        } else if(P1State[1][0] == P1State[1][1] && P1State[1][0] == P1State[1][2]){
+ 
+            ledWinningNumbers[0] = 5;
+            ledWinningNumbers[1] = 4;
+            ledWinningNumbers[2] = 3;
+            return 1;
+ 
+        } else if(P1State[2][0] == P1State[2][1] && P1State[2][0] == P1State[2][2]){
+ 
+            ledWinningNumbers[0] = 6;
+            ledWinningNumbers[1] = 7;
+            ledWinningNumbers[2] = 8;
+            return 1;
+ 
+        } else if(P1State[0][0] == P1State[1][0] && P1State[0][0] == P1State[2][0]){
+ 
+            ledWinningNumbers[0] = 0;
+            ledWinningNumbers[1] = 5;
+            ledWinningNumbers[2] = 6;
+            return 1;
+ 
+        } else if(P1State[0][1] == P1State[1][1] && P1State[0][1] == P1State[2][1]){
+ 
+            ledWinningNumbers[0] = 1;
+            ledWinningNumbers[1] = 4;
+            ledWinningNumbers[2] = 7;
+            return 1;
+ 
+        } else if(P1State[0][2] == P1State[1][2] && P1State[0][2] == P1State[2][2]){
+ 
+            ledWinningNumbers[0] = 2;
+            ledWinningNumbers[1] = 3;
+            ledWinningNumbers[2] = 8;
+            return 1;
+ 
+        } else if(P1State[0][0] == P1State[1][1] && P1State[0][0] == P1State[2][2]){
+ 
+            ledWinningNumbers[0] = 0;
+            ledWinningNumbers[1] = 4;
+            ledWinningNumbers[2] = 8;
+            return 1;
+ 
+        } else if(P1State[0][2] == P1State[1][1] && P1State[0][2] == P1State[2][0]){
+ 
+            ledWinningNumbers[0] = 2;
+            ledWinningNumbers[1] = 4;
+            ledWinningNumbers[2] = 6;
+            return 1;
+
+        }
+ else {
+            
+            // Determine whether or not the game has finished using for loops
+            bool finishedGame = true; // This will detect whether or not all iLED's are on
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if ( ( P1State[i][j] && P2State[i][j] ) == false ) {
+                    finishedGame = false;
+                    }
+                }
+            }
+            if (finishedGame == true) {
+                return -2; // The game has finished, nobody has won
+            }
+            else {
+            return -1; // The game has yet to finish
+            }
+        }
+}
+/*
+Description: To be used when a player has won a game, this function will blink the winning LED's three times . NOTE: Uses delay() blocking code to do the blinking
+Inputs:
+    N/A
+Outputs:
+    N/A
+Global variables used:
+strip.setPixelColor(iLed Number, Pixel Color) used to change the colors of the LED's
+ledWinningNumbers array used to determine which iLed's to blink.
+*/
+void blinkLED() {
+    // For loop to repeat this process three times
+    for (int i = 0; i < 3; i++) {
+    strip.setPixelColor(ledWinningNumbers[0],PixelColorRed);
+    strip.setPixelColor(ledWinningNumbers[1],PixelColorRed);
+    strip.setPixelColor(ledWinningNumbers[2],PixelColorRed);
+    strip.show();
+    delay(300); // Delay for .3 seconds
+    strip.setPixelColor(ledWinningNumbers[0],PixelColorOff);
+    strip.setPixelColor(ledWinningNumbers[1],PixelColorOff);
+    strip.setPixelColor(ledWinningNumbers[2],PixelColorOff);
+    strip.show();
+    delay(300); // Delay for .3 seconds
+    }
+}
+/*
+Description: To be used when neither players have won a game, this function will blink all LED's three times. NOTE: Uses delay() blocking code to do the blinking. Will blink green when both lose
+Inputs:
+    N/A
+Outputs:
+    N/A
+Global variables used:
+strip.setPixelColor(iLed Number, Pixel Color) used to change the colors of the LED's
+*/
+void blinkAllLEDS() {
+    // For loop to repeat this process three times
+    for (int i = 0; i < 9; i++) {
+    strip.setPixelColor(i,PixelColorRed);
+    }
+    strip.show();
+    delay(300); // Delay for .3 seconds
+    for (int i = 0; i < 9; i++) {
+    strip.setPixelColor(i,PixelColorOff);
+    }
+    strip.show();
+    delay(300); // Delay for .3 seconds
+}
+
+// --- Handler Functions ---
